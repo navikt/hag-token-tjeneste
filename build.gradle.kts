b7aa3f8f5ea52ef3721a0dbfd996b087c2a21403
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-10-17 15:30:17 +0200

    bruk dev og refaktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
 val kotlin_version: String by project
 val logback_version: String by project
-val mockk_version: String by project
-val arbeidsgiver_notifikasjon_klient_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.20"
@@ -37,23 +35,13 @@ dependencies {
     implementation("io.ktor:ktor-client-apache5")
     implementation("io.ktor:ktor-client-content-negotiation")
     implementation("io.ktor:ktor-client-core")
-//    implementation("io.ktor:ktor-client-plugins")
-//    implementation("io.ktor:ktor-client-logging")
-//    implementation("io.ktor:ktor-client-auth")
-    implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("io.ktor:ktor-server-auth-jvm")
     implementation("io.ktor:ktor-server-auth-jwt-jvm")
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-html-builder")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("no.nav.helsearbeidsgiver:arbeidsgiver-notifikasjon-klient:$arbeidsgiver_notifikasjon_klient_version")
-    implementation("no.nav.helsearbeidsgiver:utils:0.9.0")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-css:1.0.0-pre.817")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
 
     testImplementation("io.ktor:ktor-client-core")
-//    testImplementation("io.ktor:ktor-client-plugins")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    testImplementation("io.mockk:mockk:$mockk_version")
 }
