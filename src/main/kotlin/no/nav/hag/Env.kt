9b210e0eff00c703104732d900bd7ca1c4ba7519
Jesper Hustad <jesper.forrest.hustad@nav.no>
2025-10-23 13:21:13 +0200

    Sletter dotenv støtte

--- src/main/kotlin/no/nav/hag/Env.kt
+++ src/main/kotlin/no/nav/hag/Env.kt
@@ -1,16 +1,7 @@
 package no.nav.hag
 
-import io.github.cdimascio.dotenv.dotenv
-
-private val dotenv =
-    dotenv {
-        ignoreIfMalformed = true
-        ignoreIfMissing = true
-    }
-
-val maskinportenIntegrasjonsId: String = System.getenv("MASKINPORTEN_INTEGRATION_ID") ?: dotenv["MASKINPORTEN_INTEGRATION_ID"]
-val maskinportenKid: String = System.getenv("MASKINPORTEN_KID") ?: dotenv["MASKINPORTEN_KID"]
-val maskinportenTokenEndpoint: String = System.getenv("MASKINPORTEN_TOKEN_ENDPOINT") ?: dotenv["MASKINPORTEN_TOKEN_ENDPOINT"]
-val maskinportenClientIssuer: String = System.getenv("MASKINPORTEN_CLIENT_ISSUER") ?: dotenv["MASKINPORTEN_CLIENT_ISSUER"]
-val maskinportenPrivateKey: String =
-    System.getProperty("MASKINPORTEN_PKEY") ?: System.getenv("MASKINPORTEN_PKEY") ?: dotenv["MASKINPORTEN_PKEY"]
+val maskinportenIntegrasjonsId: String = System.getenv("MASKINPORTEN_INTEGRATION_ID")
+val maskinportenKid: String = System.getenv("MASKINPORTEN_KID")
+val maskinportenTokenEndpoint: String = System.getenv("MASKINPORTEN_TOKEN_ENDPOINT")
+val maskinportenClientIssuer: String = System.getenv("MASKINPORTEN_CLIENT_ISSUER")
+val maskinportenPrivateKey: String = System.getProperty("MASKINPORTEN_PKEY") ?: System.getenv("MASKINPORTEN_PKEY")
